a6d4a19b96280c009d6a9b0a111f441d3f4a6266
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-09-20 00:06:03 +0530

    seat configuration based on color

--- src/Components/CininmaHallBooking.jsx
+++ src/Components/CininmaHallBooking.jsx
@@ -21,12 +21,49 @@ const CininmaHallBooking = ({
     },
   },
   bookedSeats = [],
-  cuurency = "INR",
+  currency = "INR",
   onBookingComplete = () => {},
   title = "Cininma Hall Booking",
   subTitle = "Select your preferred seats",
 }) => {
-  const getSeatTypesForRow = () => {};
+  const colors = [
+    "blue",
+    "purple",
+    "yellow",
+    "green",
+    "red",
+    "indigo",
+    "pink",
+    "gray",
+  ];
+  const getColorClass = (colorName) => {
+    const colorMap = {
+      blue: "bg-blue-100 border-blue-300 text-blue-800 hover:bg-blue-200",
+      purple:
+        "bg-purple-100 border-purple-300 text-purple-800 hover:bg-purple-200",
+      yellow:
+        "bg-yellow-100 border-yellow-300 text-yellow-800 hover:bg-yellow-200",
+      green: "bg-green-100 border-green-300 text-green-800 hover:bg-green-200",
+      red: "bg-red-100 border-red-300 text-red-800 hover:bg-red-200",
+      indigo:
+        "bg-indigo-100 border-indigo-300 text-indigo-800 hover:bg-indigo-200",
+      pink: "bg-pink-100 border-pink-300 text-pink-800 hover:bg-pink-200",
+      gray: "bg-gray-100 border-gray-300 text-gray-800 hover:bg-gray-200",
+    };
+    return colorMap[colorName] || colorMap.blue;
+  };
+  const getSeatTypesForRow = (row) => {
+    const seatTypeEntries = Object.entries(seatTypes);
+    for (let i = 0; i < seatTypeEntries.length; i++) {
+      const [type, config] = seatTypeEntries[i];
+      if (config.rows.includes(row)) {
+        const color = colors[i % colors.length];
+        return { type, color, ...config };
+      }
+    }
+    const [firstType, firstConfig] = seatTypeEntries[0];
+    return { type: firstType, color: colors[0], ...firstConfig };
+  };
   const initializedSeats = useMemo(() => {
     const seats = [];
     for (let row = 0; row < layout.rows; row++) {
@@ -49,8 +86,42 @@ const CininmaHallBooking = ({
     }
     return seats;
   }, [layout, seatTypes, bookedSeats]);
-
+  const handleSeatClick = () => {};
   const [seats, setSeats] = useState(initializedSeats);
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const getSeatClassName = (seat) => {
+    const baseClass = `w-8 h-8 sm:w-10 lg:w-12 lg:h-12 m-1 rounded-t-lg border-2 cursor-pointer transition-all duration-200 flex items-center justify-center text:xs sm:text-sm font-bold bg-blue-100 border-blue-300 text-blue-800`;
+    if (seat.status === "booked") {
+      return `${baseClass} bg-gray-400 border-gray-300 text-gray-600 cursoe-not-allowed`;
+    }
+    if (seat.selected) {
+      return `${baseClass} border-green-600 bg-green-500 text-gray-600 text-white transform scale-110`;
+    }
+    return `${baseClass} ${getColorClass(seat.color)}`;
+  };
+  const renderSeatSelection = (seatRow, startIndex, endIndex) => {
+    return (
+      <div className="flex items-center">
+        {seatRow.slice(startIndex, endIndex).map((seat, index) => {
+          return (
+            <div
+              key={seat.id}
+              className={getSeatClassName(seat)}
+              title={`${seat.id} - ${
+                getSeatTypesForRow(seat.row)?.name || "Regular"
+              }- ${seat.price} ${currency}`}
+              onClick={() => {
+                handleSeatClick(seat.row, startIndex + index);
+              }}
+            >
+              {startIndex + index + 1}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <div className="w-full min-h-screen bg-gray-50 p-4 ">
       {/* title */}
@@ -75,6 +146,14 @@ const CininmaHallBooking = ({
                   <span className="w-8 text-center font-bold text-gray-600 mr-4">
                     {String.fromCharCode(65 + rowIndex)}
                   </span>
+                  {renderSeatSelection(row, 0, layout.aislePosition)}
+                  {/* isleOptions */}
+                  <div className="w-8"></div>
+                  {renderSeatSelection(
+                    row,
+                    layout.aislePosition,
+                    layout.seatsPerRow
+                  )}
                 </div>
               );
             })}
